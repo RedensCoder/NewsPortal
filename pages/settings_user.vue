3d42972b8e80b6846a887907385633a389b5fabf
lil-qlich <127847622+lil-qlich@users.noreply.github.com>
2024-01-28 15:03:51 +0000

    помогитн

--- pages/settings_user.vue
+++ pages/settings_user.vue
@@ -1,35 +1,45 @@
 <script></script>
 <template>
+    <Articles />
     <Populars />
-    <div class="main">
-    <div class="header">
+    <div class="heads">
         <h1 class="head_txt">Настройки</h1>
     </div>
     <div class="settings">
-        <img class="icon_photo" src="/img/add_picture.png" alt="">
+        <div class="icon_photo">
+        <img class="icon" src="/img/add_picture.png" alt="">
+        </div>
     </div>
-</div>
+
 </template>
 <style scoped>
-.header{
-    width: 70%;
-    margin-left: 15%;
-    margin-right: 15%;
+.heads{
+    width: 50%;
+    margin-left: 20%;
     margin-top: 3%;
     justify-content: center;
     height: 10vh;
-    background-color: white;
+    background-color: #FFFFFF;
 }
 .head_txt{
     
 }
 .settings{
-    width: 70%;
+    width: 50%;
     margin-top: 3%;
-    margin-left: 15%;
-    background-color: white;
+    margin-left: 20%;
+    height: 100vh;
+    background-color: #FFFFFF;
 }
 .icon_photo{
     border: 1px solid #818181;
+    width: 6.5%;
+    height: 6.5%;
+    border-radius: 50px;
+}
+.icon{
+    margin-left: 25%;
+    margin-top: 25%;
+    top: 50%;
 }
 </style>
