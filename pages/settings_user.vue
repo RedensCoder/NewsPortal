c7f0616abec9b816a462febc6e8b566d86b7cd79
lil-qlich <127847622+lil-qlich@users.noreply.github.com>
2024-01-28 11:16:39 +0000

    да

---
+++ pages/settings_user.vue
@@ -0,0 +1,35 @@
+<script></script>
+<template>
+    <Populars />
+    <div class="main">
+    <div class="header">
+        <h1 class="head_txt">Настройки</h1>
+    </div>
+    <div class="settings">
+        <img class="icon_photo" src="/img/add_picture.png" alt="">
+    </div>
+</div>
+</template>
+<style scoped>
+.header{
+    width: 70%;
+    margin-left: 15%;
+    margin-right: 15%;
+    margin-top: 3%;
+    justify-content: center;
+    height: 10vh;
+    background-color: white;
+}
+.head_txt{
+    
+}
+.settings{
+    width: 70%;
+    margin-top: 3%;
+    margin-left: 15%;
+    background-color: white;
+}
+.icon_photo{
+    border: 1px solid #818181;
+}
+</style>
