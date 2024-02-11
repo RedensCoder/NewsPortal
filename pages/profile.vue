7f8a2232afe7f769dfb9b5f754989f5b4ab16c8e
lil-qlich <127847622+lil-qlich@users.noreply.github.com>
2024-02-11 17:09:45 +0300

    профиль почти готов, осталось отредачить стили

--- pages/profile.vue
+++ pages/profile.vue
@@ -3,35 +3,9 @@
 <template>
     <Articles />
     <Populars />
-    <div class="profile_info">
-    <div class="icon_back">
-        <div class="icon">
-        <img class="icon2" src="/img/buda.jpg" alt="">
-        <p class="text">nickname</p>
-        <p class="text">публикую всякую всячину.</p>
-        </div>
-        <div class="site">
-            <p class="text">Соц.Сети:</p>
-            <a href=""><img src="" alt=""></a>
-            <a href=""><img src="" alt=""></a>
-        </div>
-    </div>
-</div>
+
 </template>
 
 <style scoped>
-.icon_back{
-        background-color: #8657E9;
-        width: 50%;
-        margin-left: 22.5%;
-    }
-.icon2{
-    width: 12.5%;
-    height: 12.5%;
-    border-radius: 58px;
-    border: 1px solid #FFFFFF;
-}
-.profile_info{
-    margin-top: 1.7%;
-}
+
 </style>
